09cbd982440d96a0741963d4d90421faabe47c8a
prince2133 <ashuunew4@gmail.com>
2025-04-19 15:26:26 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const APIs = {
+  1:'https://apkcombo.com',
+  2:'apk-dl.com',
+  3:'https://apk.support',
+  4:'https://apps.evozi.com/apk-downloader',
+  5:'http://ws75.aptoide.com/api/7',
+  6:"https://cafebazaar.ir"
 }
+const Proxy = (url)=>(url ? `https://translate.google.com/translate?sl=en&tl=fr&hl=en&u=${encodeURIComponent(url)}&client=webapp`: '')
+
+const api = (ID, path = '/', query = {}) => (ID in APIs ? APIs[ID] : ID) + path + (query ? '?' + new URLSearchParams(Object.entries({...query})) : '')
 
 
-module.exports = {
-SESSION_ID: 'ZEROTWO=kmsRFKBD#CSMW_N13TIMTA3i5fUHIoXqX7XGrNUKCgxtz34QmI2M'
-};
+module.exports = { APIs, Proxy, api}
